b42178e30dce5eab767ea5371485e8ee17b51b2c
MrPuel <43436212+MrPuel@users.noreply.github.com>
2022-03-04 17:52:59 +0100

    test commit

--- src/main/java/sncf/oui/io/core/main.java
+++ src/main/java/sncf/oui/io/core/main.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 
 public class main {
     public static void main(String[] args) {
+        int test =0;
         HashMap<Integer,String> map = new HashMap<>();
         map.put(3,"Fizz");
         map.put(5,"Buzz");
